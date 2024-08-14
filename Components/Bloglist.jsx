ce4439d037e72bb532b06c4b6acf22c36711937b
Adarsh Tiwari <tiwariadarsh9910@gmail.com>
2024-08-14 15:50:31 +0530

    update2

--- Components/Bloglist.jsx
+++ Components/Bloglist.jsx
@@ -25,7 +25,7 @@ const Bloglist = () => {
         <button onClick={()=>setMenu('Startup')} className={menu==="Startup"?"bg-black text-white py-1 px-4 rounded-sm":""}>Startup</button>
         <button onClick={()=>setMenu('Lifestyle')} className={menu==="Lifestyle"?"bg-black text-white py-1 px-4 rounded-sm":""}>Lifestyle</button>
       </div>
-      <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
+      <div className=' max-w-[100vw] flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
         {
             blogs.filter((item)=>menu==="All"?true:item.category===menu  ).map((item,index)=>{
                  return <Blogitem key={index} id={item._id}image={item.image} title={item.title} description={item.description} category={item.category}/>
